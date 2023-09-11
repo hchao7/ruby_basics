97a90457dff6c2ea1f5421f3821cc0d5d5158f56
Helen Chao <helenwchao01@gmail.com>
2023-09-11 15:48:35 -0700

    Complete Exercise 3 and 4

--- Chapter_2_Variables/name.rb
+++ Chapter_2_Variables/name.rb
@@ -1,3 +1,21 @@
+puts "Exercise 1"
 puts "Please type in your name."
 name = gets.chomp
 puts name + ", nice to see you around these parts!"
+
+# print the name of user 10 times in a row
+puts "Exercise 3"
+10.times do puts name end
+
+# ask user for first name, save to variable
+# do same for last name
+# output full name all at once
+
+puts "Exercise 4"
+puts "Please type in your first name."
+first_name = gets.chomp
+
+puts "Please type in your last name."
+last_name = gets.chomp
+
+puts first_name + " " + last_name
