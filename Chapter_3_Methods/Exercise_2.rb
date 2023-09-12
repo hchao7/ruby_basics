0c6173ce843a3b8c9364250538490c1834ddbc19
Helen Chao <helenwchao01@gmail.com>
2023-09-12 14:00:53 -0700

    Complete Exercise 2

---
+++ Chapter_3_Methods/Exercise_2.rb
@@ -0,0 +1,17 @@
+x = 2
+# returns 2
+
+puts x = 2
+# output: 2
+# returns 2
+
+p name = "Joe"
+# output: "Joe"
+# returns "Joe"
+
+four = "four"
+# returns "four"
+
+print something = "nothing"
+# output: "nothing"
+# returns nil
