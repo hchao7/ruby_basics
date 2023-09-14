d597be944ddefa53133c34ae839bd5b6a5022dd1
Helen Chao <helenwchao01@gmail.com>
2023-09-14 13:56:43 -0700

    Complete Exercise 6

---
+++ Chapter_4_Flow_Control/Exercise_6.rb
@@ -0,0 +1,6 @@
+(32*4) >= "129" # False, can't compare two diff types
+847 == '847' # False, can't compare two diff types
+'847' < '846' # False, 7 > 6
+'847' > '846' # True
+'847' > '8478' # False
+'847' < '8478' # True
