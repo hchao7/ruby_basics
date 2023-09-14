ebe3b7c150e3a30ee39ddb8050b5322d74731e72
Helen Chao <helenwchao01@gmail.com>
2023-09-14 11:53:49 -0700

    Update Exercise 3 and complete Exercise 4

--- Chapter_4_Flow_Control/Exercise_3.rb
+++ Chapter_4_Flow_Control/Exercise_3.rb
@@ -2,10 +2,34 @@
 puts "Please, enter a number between 0 and 100: "
 number = gets.chomp.to_i
 
+# Original Version 
 case
 when number == 49
   puts "between 0 and 50"
 when number >= 51 && number <= 100
+  puts "between 51 and 100"
+else
+  puts "above 100"
+end
+
+puts "Other Versions"
+
+# Have argument and save result in a variable
+result = case number
+when 0..50
+  "between 50 and 100"
+when 51..101
+  "between 51 and 100"
+else
+  "above 100"
+end
+puts result
+
+# Have no argument
+case
+when (0..50) === number
+  puts "between 50 and 100"
+when (51..101) === number
   puts "between 51 and 100"
 else
   puts "above 100"
