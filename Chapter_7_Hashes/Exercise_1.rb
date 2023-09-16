7260db2ab61370811f0ebba7ecd0b523bea34440
Helen Chao <helenwchao01@gmail.com>
2023-09-16 12:17:24 -0700

    Add Chapter 7 folder and complete Exercise 1

---
+++ Chapter_7_Hashes/Exercise_1.rb
@@ -0,0 +1,9 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+        
+siblings_hash = family.select { |k,v| (k == :sisters) || (k == :brothers) }
+siblings_array = siblings_hash.values.flatten
+p siblings_array
