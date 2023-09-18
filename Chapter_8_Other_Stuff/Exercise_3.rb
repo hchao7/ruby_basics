6a80e24c0030e6d90bd68dcc006626748e1e1c51
Helen Chao <helenwchao01@gmail.com>
2023-09-17 17:58:34 -0700

    Complete Exercise 3

---
+++ Chapter_8_Other_Stuff/Exercise_3.rb
@@ -0,0 +1,5 @@
+# What is exception handling and what problem does it solve?
+
+=begin Exception handling = a structure used to handle the possibility of
+an error occuring. It handles the error by changing the flow of control without
+exiting the program entirely.
