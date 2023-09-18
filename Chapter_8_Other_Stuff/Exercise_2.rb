b656518536b3e7d780526407d4b011a2105b2368
Helen Chao <helenwchao01@gmail.com>
2023-09-17 18:00:05 -0700

    Update Exercise 2 with Exercise 4's additions

--- Chapter_8_Other_Stuff/Exercise_2.rb
+++ Chapter_8_Other_Stuff/Exercise_2.rb
@@ -1,5 +1,15 @@
+=begin
+
 def execute(&block)
   block
+end 
+
+execute { puts "Hello from inside the execute method!"}
+
+=end
+
+def execute(&block)
+  block.call
 end 
 
 execute { puts "Hello from inside the execute method!"}
